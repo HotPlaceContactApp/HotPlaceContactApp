e161bb0e628528fb970222006ed803328aff09e3
Young Park <minyeoungpark6929@gmail.com>
2024-01-15 14:27:34 +0900

    Hot place contact App initial commit

---
+++ app/src/main/java/com/example/hotplacecontactapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hotplacecontactapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// 10조 맛집 연락처 앱 파이팅!!
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
