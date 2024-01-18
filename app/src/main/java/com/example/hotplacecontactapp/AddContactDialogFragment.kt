25946a7a146c69596b325249380050258fd150d5
avo890 <wizhey7327@gmail.com>
2024-01-18 21:27:59 +0900

    [FEAT]: EventNotification 기능 구현

--- app/src/main/java/com/example/hotplacecontactapp/AddContactDialogFragment.kt
+++ app/src/main/java/com/example/hotplacecontactapp/AddContactDialogFragment.kt
@@ -1,17 +1,23 @@
 package com.example.hotplacecontactapp
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.DialogFragment
 import com.example.hotplacecontactapp.databinding.AddcontactLayoutDialogBinding
 
@@ -21,7 +27,6 @@ class AddContactDialogFragment : DialogFragment() {
     private var _binding: AddcontactLayoutDialogBinding? = null
     private val binding get() = _binding!!
     var listener: AddContactListener? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         isCancelable = true
@@ -69,11 +74,17 @@ class AddContactDialogFragment : DialogFragment() {
                     isFavorite = false
                 )
 
+
                 validAdd(addData)
             }
 
             addContactBtnNegative.setOnClickListener {
                 dismiss()
+            }
+
+            val handler = Handler(Looper.getMainLooper())
+            addContactBtnNotification.setOnClickListener {
+                handler.postDelayed({ eventNotification() }, 3000)
             }
         }
     }
@@ -130,6 +141,42 @@ class AddContactDialogFragment : DialogFragment() {
             listener?.onContactAdded()
             dismiss()
         }
+    }
+
+    fun eventNotification() {
+        val manager = activity?.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+
+        val channelId = "one-channel"
+        val channelName = "My Channel One"
+        val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT).apply {
+            description = "My Channel One Description"
+            setShowBadge(true)
+            val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+            setSound(uri, audioAttributes)
+            enableVibration(true)
+        }
+
+        manager.createNotificationChannel(channel)
+        builder = NotificationCompat.Builder(requireContext(), channelId)
+
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(requireContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+        builder.run {
+            setSmallIcon(R.mipmap.ic_launcher)
+            setWhen(System.currentTimeMillis())
+            setContentTitle("연락처 알림")
+            setContentText("설정한 알림이 도착했습니다!!")
+            setContentIntent(pendingIntent)
+            setAutoCancel(true)
+        }
+        manager.notify(11, builder.build())
     }
 
 }
