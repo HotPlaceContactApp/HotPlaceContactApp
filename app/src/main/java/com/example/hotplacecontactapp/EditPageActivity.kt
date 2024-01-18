7b1c45fe8d3577bc29bd71d7587584cacc6ecfa5
이성재 <sj5lee1@gmail.com>
2024-01-18 12:05:16 +0900

    [FEAT]: added back button functionality [FEAT]: save button can send data [FEAT]: added name and phone check

--- app/src/main/java/com/example/hotplacecontactapp/EditPageActivity.kt
+++ app/src/main/java/com/example/hotplacecontactapp/EditPageActivity.kt
@@ -1,6 +1,9 @@
 package com.example.hotplacecontactapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hotplacecontactapp.databinding.ActivityEditPageBinding
 
@@ -10,5 +13,37 @@ class EditPageActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        closeBtncheck()
+        saveBtncheck()
+//        addImgBtncheck()
+    }
+
+    private fun closeBtncheck() {
+        binding.imgBack.setOnClickListener {
+            finish()
+        }
+        binding.textExitButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun saveBtncheck() {
+        binding.textSaveButton.setOnClickListener {
+            if (binding.textName.text.isEmpty()) {
+                Toast.makeText(this, "이름을 입력하세요.", Toast.LENGTH_SHORT).show()
+            } else if (binding.textPhoneNumber.text.isEmpty()) {
+                Toast.makeText(this, "휴대전화 번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                var intent = Intent(this, EditPageActivity::class.java)
+                intent.putExtra("test", binding.textName.text.toString())
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+        }
+    }
+
+    private fun addImgBtncheck() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/hotplacecontactapp/MyPageFragment.kt
+++ app/src/main/java/com/example/hotplacecontactapp/MyPageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.hotplacecontactapp
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.hotplacecontactapp.databinding.FragmentMyPageBinding
 
 private const val ARG_PARAM1 = "param1"
@@ -18,6 +20,13 @@ class MyPageFragment : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            binding.textName.text=data?.getStringExtra("test")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,14 +51,14 @@ class MyPageFragment : Fragment() {
     }
 
     private fun initView() {
-        updateProfileData()
-        checkPfp()
-        addData()
-        checkVisablity()
+//        updateProfileData()
+//        checkPfp()
+//        addData()
+//        checkVisablity()
 
         binding.textEdit.setOnClickListener {
             val intent = Intent(activity, EditPageActivity::class.java)
-            startActivity(intent)
+            resultLauncher.launch(intent)
         }
         binding.cardCall.setOnClickListener {
             val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${binding.textPhoneNumber.text}"))
