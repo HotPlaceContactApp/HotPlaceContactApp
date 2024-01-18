983832728fd65afc041782dffdf8ced9d19470a5
SoungHyun Choi <sounghyun35@naver.com>
2024-01-18 12:47:17 +0900

    fragment data transmit

--- app/src/main/java/com/example/hotplacecontactapp/FavoriteListFragment.kt
+++ app/src/main/java/com/example/hotplacecontactapp/FavoriteListFragment.kt
@@ -1,40 +1,46 @@
 package com.example.hotplacecontactapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.hotplacecontactapp.databinding.FragmentMyPageBinding
+import com.example.hotplacecontactapp.databinding.FragmentContactListBinding
 
 private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-class MyPageFragment : Fragment() {
-    private var _binding: FragmentMyPageBinding? = null
+
+class FavoriteListFragment : Fragment() {
+    private var _binding: FragmentContactListBinding? = null
+
     private val binding get() = _binding!!
 
     private var param1: String? = null
+    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        _binding = FragmentContactListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        initView()
+        initView()
     }
 
     private fun initView() {
@@ -49,9 +55,10 @@ class MyPageFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance(param1: String) =
-            MyPageFragment().apply {
+            FavoriteListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
             }
     }
